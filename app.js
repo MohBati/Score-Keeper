2da159bee1ae0d25ab93bb0bea3945c9e9c1fe52
MohSas <m.abdallahsas@gmail.com>
2025-01-05 21:43:37 +0300

    again

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const p1Score = document.querySelector('#p1Score');
-const p2Score = document.querySelector('#p2Score');
-const reset = document.querySelector('#reset')
+const p1Button = document.querySelector('#p1Button')
+const p2Button = document.querySelector('#p2Button')
+const p1Display = document.querySelector('#p1Display')
+const p2Display = document.querySelector('#p2Display')
 
+let p1Score = 0;
 
-let p1Counter = 0;
-let p2Counter = 0;
-let winningScore = 5;
+let isGameOver = false;
 
-p1Button.addEventListener('click', function() {
-    if (p1Counter !== winningScore){
-        p1Counter+= 1;
-        p1Score.textContent = p1Counter;
-    }
-})
-
-p2Button.addEventListener('click', function() {
-    if (p2Counter !== winningScore){
-    p2Counter+= 1;
-    p2Score.textContent = p2Counter;
-    }
-})
-
-reset.addEventListener('click', function() {
-    p1Score.inne = '0';
-    p2Score.textContent = '0';
-    p1Counter = 0;
-    p2Counter = 0;
+p1Button.addEventListener('click', () => {
+    p1Score += 1;
+    p1Display.textContent = p1Score;
 })
