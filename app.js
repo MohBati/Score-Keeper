0880db65390a152c42d4c234bd116a6590608738
MohSas <m.abdallahsas@gmail.com>
2025-01-05 21:55:45 +0300

    two buttons works greatly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
 
 let p1Score = 0;
-
+let p2Score = 0;
 let isGameOver = false;
 let winningScore = 5;
 
@@ -15,5 +15,15 @@ p1Button.addEventListener('click', () => {
             isGameOver = true;
         }
         p1Display.textContent = p1Score;
+    }
+})
+
+p2Button.addEventListener('click', () => {
+    if (!isGameOver){
+        p2Score += 1;
+        if (p2Score === winningScore){
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
     }
 })
