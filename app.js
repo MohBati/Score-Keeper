4eb50f5e8a78142f39a92ad5379da162c0672e01
MohSas <m.abdallahsas@gmail.com>
2025-01-05 22:04:06 +0300

    reset button works greatly

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
+const resetBtn = document.querySelector('#reset')
 
 let p1Score = 0;
 let p2Score = 0;
@@ -26,4 +27,12 @@ p2Button.addEventListener('click', () => {
         }
         p2Display.textContent = p2Score;
     }
+})
+
+resetBtn.addEventListener('click', () => {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    isGameOver = false;
 })
